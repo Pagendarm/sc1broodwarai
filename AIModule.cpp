322f71ec3d5763a8d66fcbbb0cc87d224e4cad45
Leif Myer <chatsbow@aol.com>
2013-11-26 12:49:29 -0800

    AIModule Files
    You may not be able to run the code but at least this will allow you to
    see the structure of the AIModule that will control the Dragoons

---
+++ AIModule.cpp
@@ -0,0 +1,167 @@
+#include "AIModule.h"
+#include <iostream>
+
+using namespace BWAPI;
+using namespace Filter;
+
+void ExampleAIModule::onStart()
+{
+  // Hello World!
+  Broodwar->sendText("Hello world!");
+
+  // Print the map name.
+  // BWAPI returns std::string when retrieving a string, don't forget to add .c_str() when printing!
+  Broodwar << "The map is " << Broodwar->mapName() << "!" << std::endl;
+
+  // Enable the UserInput flag, which allows us to control the bot and type messages.
+  Broodwar->enableFlag(Flag::UserInput);
+
+  // Uncomment the following line and the bot will know about everything through the fog of war (cheat).
+  //Broodwar->enableFlag(Flag::CompleteMapInformation);
+
+  // Set the command optimization level so that common commands can be grouped
+  // and reduce the bot's APM (Actions Per Minute).
+  Broodwar->setCommandOptimizationLevel(2);
+  
+  // Any additional starter code here!!!
+
+}
+
+void ExampleAIModule::onEnd(bool isWinner)
+{
+  // Called when the game ends
+  if ( isWinner )
+  {
+    // Log your win here!
+  }
+}
+
+void ExampleAIModule::onFrame()
+{
+  // Called once every game frame
+
+  // Display the game frame rate as text in the upper left area of the screen
+  Broodwar->drawTextScreen(200, 0,  "FPS: %d", Broodwar->getFPS() );
+  Broodwar->drawTextScreen(200, 20, "Average FPS: %f", Broodwar->getAverageFPS() );
+
+  // Return if the game is a replay or is paused
+  if ( Broodwar->isReplay() || Broodwar->isPaused() || !Broodwar->self() )
+    return;
+
+  // Prevent spamming by only running our onFrame once every number of latency frames.
+  // Latency frames are the number of frames before commands are processed.
+  if ( Broodwar->getFrameCount() % Broodwar->getLatencyFrames() != 0 )
+    return;
+
+  // Iterate through all the units that we own
+  Unitset myUnits = Broodwar->self()->getUnits();
+  for ( Unitset::iterator u = myUnits.begin(); u != myUnits.end(); ++u )
+  {
+    // Ignore the unit if it no longer exists
+    // Make sure to include this block when handling any Unit pointer!
+    if ( !u->exists() )
+      continue;
+
+    // Ignore the unit if it has one of the following status ailments
+    if ( u->isLockedDown() || u->isMaelstrommed() || u->isStasised() )
+      continue;
+
+    // Ignore the unit if it is in one of the following states
+    if ( u->isLoaded() || !u->isPowered() || u->isStuck() )
+      continue;
+
+    // Ignore the unit if it is incomplete or busy constructing
+    if ( !u->isCompleted() || u->isConstructing() )
+      continue;
+
+
+    // ~~~Behavior tree Logic here!!!
+
+
+
+  } // closure: unit iterator
+}
+
+void ExampleAIModule::onSendText(std::string text)
+{
+
+  // Send the text to the game if it is not being processed.
+  Broodwar->sendText("%s", text.c_str());
+
+
+  // Make sure to use %s and pass the text as a parameter,
+  // otherwise you may run into problems when you use the %(percent) character!
+
+}
+
+void ExampleAIModule::onReceiveText(BWAPI::Player player, std::string text)
+{
+  // Parse the received text
+  Broodwar << player->getName() << " said \"" << text << "\"" << std::endl;
+}
+
+void ExampleAIModule::onPlayerLeft(BWAPI::Player player)
+{
+  // Interact verbally with the other players in the game by
+  // announcing that the other player has left.
+  Broodwar->sendText("Goodbye %s!", player->getName().c_str());
+}
+
+void ExampleAIModule::onNukeDetect(BWAPI::Position target)
+{
+}
+
+void ExampleAIModule::onUnitDiscover(BWAPI::Unit unit)
+{
+	// Called when a unit is seen
+	// Probably won't need logic here if everything runs in onFrame()
+}
+
+void ExampleAIModule::onUnitEvade(BWAPI::Unit unit)
+{
+}
+
+void ExampleAIModule::onUnitShow(BWAPI::Unit unit)
+{
+	// Called when a previously seen unit is seen again
+}
+
+void ExampleAIModule::onUnitHide(BWAPI::Unit unit)
+{
+}
+
+void ExampleAIModule::onUnitCreate(BWAPI::Unit unit)
+{
+  //if ( Broodwar->isReplay() )
+  //{
+  //  // if we are in a replay, then we will print out the build order of the structures
+  //  if ( unit->getType().isBuilding() && !unit->getPlayer()->isNeutral() )
+  //  {
+  //    int seconds = Broodwar->getFrameCount()/24;
+  //    int minutes = seconds/60;
+  //    seconds %= 60;
+  //    Broodwar->sendText("%.2d:%.2d: %s creates a %s", minutes, seconds, unit->getPlayer()->getName().c_str(), unit->getType().c_str());
+  //  }
+  //}
+}
+
+void ExampleAIModule::onUnitDestroy(BWAPI::Unit unit)
+{
+}
+
+void ExampleAIModule::onUnitMorph(BWAPI::Unit unit)
+{
+}
+
+void ExampleAIModule::onUnitRenegade(BWAPI::Unit unit)
+{
+}
+
+void ExampleAIModule::onSaveGame(std::string gameName)
+{
+  Broodwar << "The game was saved to \"" << gameName << "\"" << std::endl;
+}
+
+void ExampleAIModule::onUnitComplete(BWAPI::Unit unit)
+{
+}
